ea43226e99bbe9e41c671ea9d21ee17fafe55ee3
OmarBakhsh <36277622+OmarBakhsh@users.noreply.github.com>
2018-03-25 11:04:45 +0300

    1

--- submit.php
+++ submit.php
@@ -1,5 +1,27 @@
 <?php
-if($_POST["message"]) {
-    mail("Omar_q8@msn.com", "Form to email message", $_POST["message"], "From: omar.bakhshq8@gmail.com");
+$errors = '';
+$myemail = 'omar_q8@msn.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['lastname']) ||
+   empty($_POST['contact']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$contact = $_POST['contact'];
+$lastname = $_POST['lastname'];
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"contact: $contact \n lastname \n $lastname";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $contact";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: contact-form-thank-you.html');
 }
 ?>
